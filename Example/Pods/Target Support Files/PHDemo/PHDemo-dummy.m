c0b22a2641307f7461fd912683d0be30a35f2130
hukezhu <595632239@163.com>
2020-09-21 17:20:32 +0800

    初始化项目;2C

---
+++ Example/Pods/Target Support Files/PHDemo/PHDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PHDemo : NSObject
+@end
+@implementation PodsDummy_PHDemo
+@end
---
+++ PHDemo/Classes/PHTestLog.m
@@ -0,0 +1,16 @@
+//
+//  PHTestLog.m
+//  Pods
+//
+//  Created by hukezhu on 2020/9/21.
+//
+
+#import "PHTestLog.h"
+
+@implementation PHTestLog
+
+- (void)testLog{
+    NSLog(@"当前的版本是0.1.0");
+}
+
+@end
